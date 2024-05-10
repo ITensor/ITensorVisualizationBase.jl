2908d2bc0d3fcdf80f0c79e0375b679321e56e12
Miles <miles.stoudenmire@gmail.com>
2024-05-10 14:30:01 -0400

    Add ITensorMPS as dependency and using statements

--- examples/ex_qn_mps.jl
+++ examples/ex_qn_mps.jl
@@ -1,4 +1,5 @@
 using ITensors
+using ITensorMPS
 using ITensorVisualizationBase
 
 s = siteinds("S=1/2", 5; conserve_qns=true)
--- src/visualize.jl
+++ src/visualize.jl
@@ -1,3 +1,5 @@
+using ITensorMPS: MPS
+
 #
 # Contraction sequence
 #
