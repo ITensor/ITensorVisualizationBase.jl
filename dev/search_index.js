191dd269219c2f9f114c522cc2245e3017a09e61
Documenter.jl <documenter@juliadocs.github.io>
2025-11-24 16:11:14 +0000

    build based on cd3d69e

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#ITensorVisualizationBase","page":"Home","title":"ITensorVisualizationBase","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ITensorVisualizationBase.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"#ITensorVisualizationBase.Grid","page":"Home","title":"ITensorVisualizationBase.Grid","text":"Grid\n\nGride layout.\n\n\n\n\n\n","category":"type"}]
+[{"location":"#ITensorVisualizationBase","page":"Home","title":"ITensorVisualizationBase","text":"Documentation for ITensorVisualizationBase.\n\n","category":"section"},{"location":"#ITensorVisualizationBase.Grid","page":"Home","title":"ITensorVisualizationBase.Grid","text":"Grid\n\nGride layout.\n\n\n\n\n\n","category":"type"}]
 }
